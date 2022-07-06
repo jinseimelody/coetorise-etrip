8df1c36e6e5cc4c46a2d61d22e7c9d35a71001de
khoi.ngo <khoi.ngo@gss-sol.com>
2022-07-06 19:48:41 +0700

    format webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,43 @@
-const nodeExternals = require('webpack-node-externals') 
-const path = require('path')
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
 const config = {
-    mode: "production",
-    resolve: {
-        modules: [path.resolve(__dirname, "node_modules")],
-    },
-    entry: './app.mjs',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-        libraryTarget: 'umd',
-        clean: true
-    },
-    target: "node",
-    externals: [nodeExternals()],
-    module: {
-        rules: [
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            [
-                                "@babel/preset-env",
-                                {
-                                    targets: {
-                                        browsers: ["since 2016", "ie >= 11"]
-                                    }
-                                }
-                            ]
-                        ]
-                    }
+  mode: 'production',
+  resolve: {
+    modules: [path.resolve(__dirname, 'node_modules')]
+  },
+  entry: './app.mjs',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    libraryTarget: 'umd',
+    clean: true
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    browsers: ['since 2016', 'ie >= 11']
+                  }
                 }
-            },
-        ]
-    },
-}
+              ]
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
 
-module.exports = config
+module.exports = config;
